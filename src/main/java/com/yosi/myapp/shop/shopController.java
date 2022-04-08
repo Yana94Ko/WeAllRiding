66a30a21b411fcb4ce6ebd953b22a08ba528ea15
Yana94Ko <hyunah94.ko@gmail.com>
2022-04-09 02:01:46 +0900

    shopView 생성,내 주변 위치 검색 기능 및 검색 범위/검색 후 지도 범위 조정

--- src/main/java/com/yosi/myapp/shop/shopController.java
+++ src/main/java/com/yosi/myapp/shop/shopController.java
@@ -1,15 +1,15 @@
-package com.yosi.myapp.couese;
+package com.yosi.myapp.shop;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class courseController {
-	@GetMapping("/courseCreate")
+public class shopController {
+	@GetMapping("/shopView")
 	public ModelAndView courseCreate() {
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/course/courseCreate");
+		mav.setViewName("/shop/shopView");
 		return mav;
 	}
 }
