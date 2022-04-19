d6f61829992df0d8160a13c2adcc9c4f518dbb1b
DABIN222 <req134679@naver.com>
2022-04-20 00:19:41 +0900

    마이라이딩 시작,종료일 수정

--- src/main/java/com/yosi/myapp/myRidingPagingVO.java
+++ src/main/java/com/yosi/myapp/myRidingPagingVO.java
@@ -1,13 +1,13 @@
 package com.yosi.myapp;
 
-public class RidingPagingVO {
+public class myRidingPagingVO {
 	// 페이징
-	private int onePageRecord = 9; // 한 페이지당 출력 할 페이지 수
+	private int onePageRecord = 10; // 한 페이지당 출력 할 레코드 수
 	private int pageNum = 1; // 현재 페이지
 	private int totalRecord; // 총 레코드 수
 	private int totalPage; // 총 페이지 수
 	private int offsetIndex = 0;
-	private int onePageCount = 9; // 한 번에 표시할 레코드 수
+	private int onePageCount = 20; // 한 번에 표시할 페이지 수
 	private int startPage = 1;
 
 	// 검색
